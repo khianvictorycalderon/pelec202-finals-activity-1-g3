93f9beb4f35cabe73c69ad7be90b2872665a1d97
khianvictorycalderon <ckhianvictory@gmail.com>
2026-04-07 22:28:45 +0800

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+/*
+
+  Must include (Sabi ni sir Rov sa GClass):
+    - API Integration
+    - Error Handling
+    - Loading State
+    - Environment Variable Usage
+
+*/
+
+export default function App() {
+  return (
+    <>
+      <h1 className="text-4xl font-bold text-red-600">PELEC202 Finals Activity 1</h1>
+      <p>Group 3</p>
+    </>
+  )
+}
