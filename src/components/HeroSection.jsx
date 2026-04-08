d2a285401bae8f401b016f88ed73334ce9efb9bf
khianvictorycalderon <ckhianvictory@gmail.com>
2026-04-08 10:20:59 +0800

    Added empty hero section component

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,10 @@
+// De Leon & Talento
+export default function HeroSection() { // Do not modify this line of code, leave it as this is. Nothing inside the parenthesis.
+    // Modify only inside the return()
+    // Make it look good and aesthetic as much as possible, text aligned to left, and one button labeled "Get Started"
+    return (
+        <div>
+            This is a Hero Section. No Props required.
+        </div>
+    )
+}
